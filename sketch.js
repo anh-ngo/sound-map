197d232a6a23b524c144a8071dcdec62be6b288f
anh-ngo <thuyanhngo259@gmail.com>
2023-05-30 14:00:22 +0300

    ~

--- sketch.js
+++ sketch.js
@@ -5,17 +5,22 @@ boundaryCloseButton.addEventListener('click', function() {
 });
 
 let x, y, fence, userIcon;
-let [ latMin, latMax, lonMin, lonMax ] = [ 60.19520, 60.20036, 25.12978, 25.13839 ]
-let polygonsData, fences = [], images = [], audioFiles = [], userLocationAvailable = false;
+let [ latMin, latMax, lonMin, lonMax ] = [ 60.19528, 60.20006, 25.13053, 25.13762 ]
+let [ isInside, userLocationAvailable ] = [ false, false ];
+let polygonsData, fences = [], images = [], audioFiles = [];
 let imageSizes = [[611, 1058], [524, 479], [327, 290]];
+let audioNames = ['test1.mp3', 'test2.mp3', 'test3.mp3'];
+let audioTimers = new Array(audioNames.length);
+
 
 function preload() {
-  polygonsData = loadJSON("./assets/data-1.json");
-  audioFiles = ['test1.mp3', 'test2.mp3', 'test3.mp3'].map((file) =>
-    loadSound('./assets/' + file, 
-      () => console.log(`${file} loaded successfully`), 
-      (err) => console.error(`Failed to load ${file}`, err))
-  );
+  for (let i = 0; i < audioNames.length; i++) {
+    audioFiles[i] = loadSound('./assets/' + audioNames[i], 
+      () => { console.log('Audio loaded successfully'); }, 
+      (err) => { console.error(err); }
+    );
+  }
+  polygonsData = loadJSON("./assets/data-1.json", dataLoaded);
   images[0] = loadImage('./assets/area1.png'); 
   images[1] = loadImage('./assets/area2.png');
   images[2] =  loadImage('./assets/area3.png');
@@ -29,21 +34,23 @@ function dataLoaded(data) {
 function setup() {
   let canvas = createCanvas(500,800);
   canvas.parent("canvas-container");
+
+  watchOptions = {
+   enableHighAccuracy: true,
+   timeout: 500,
+   maximumAge: 0
+  };
   
-  watchPosition(positionChanged, {
-    enableHighAccuracy: false, //"false" helps to fetch the location faster
-    timeout: 500,
-    maximumAge: 0
-  });
+  watchPosition(positionChanged, watchOptions);
   
-polygonsData.features.forEach((polygon, index) => {
+  polygonsData.forEach((polygon, index) => {
     let fence = new geoFencePolygon(
-      polygon.geometry.coordinates[0],
+      polygon.geometry.coordinates[0], // Reverse the order of coordinates
       () => insideThePolygon(index),
       () => outsideThePolygon(index),
-      'mi'
+      'km'
     );
-    fences.push(fence);
+    fences.push({ points: polygon.geometry.coordinates[0], fence: fence });
   });
 
   // Add event listener to the HTML button for getting user interaction to start audio
@@ -51,6 +58,8 @@ polygonsData.features.forEach((polygon, index) => {
     userStartAudio();
     this.style.display = 'none';
   });
+
+  // simulatePositionChange(60.19846, 25.13218); // A point inside a known geofence
 }
 
 function positionChanged(position) {
@@ -68,16 +77,6 @@ if (lat >= latMin && lat <= latMax && lon >= lonMin && lon <= lonMax) {
   x = map(lon, lonMin, lonMax, 0, width);
   y = map(lat, latMin, latMax, height, 0);
 
-  // Check if the user is inside any geofence
-  let insideAnyPolygon = false;
-  for (let i = 0; i < fences.length; i++) {
-    if (fences[i].insideFence) {
-      insideAnyPolygon = true;
-      insideThePolygon(i); // Play sound corresponding to the polygon index
-    } else {
-      outsideThePolygon(i); // Stop playing sound for the polygon index
-    }
-  }
   // Hide the popup
   boundaryPopup.style.display = 'none';
   userLocationAvailable = true;
@@ -86,12 +85,25 @@ if (lat >= latMin && lat <= latMax && lon >= lonMin && lon <= lonMax) {
   boundaryPopup.style.display = 'flex';
   userLocationAvailable = false;
 }
+
+// Check if the user is inside any geofence
+let insideAnyPolygon = false;
+for (let i = 0; i < fences.length; i++) {
+  if (fences[i].insideFence) {
+    insideAnyPolygon = true;
+    insideThePolygon(i); // Play sound corresponding to the polygon index
+    console.log("User is inside geofence: " + i); 
+  } else {
+    outsideThePolygon(i); // Stop playing sound for the polygon index
+    console.log("User is not inside any geofence");
+  }
+}
 }
 
 function draw(){
   background("#ebdfc5"); 
 
-  polygonsData.features.forEach((polygon, index) => {
+  polygonsData.forEach((polygon, index) => {
     drawShapeFromJSON(polygon, index);
   });
 
@@ -99,7 +111,7 @@ function draw(){
     push();
     translate(x, y);
     imageMode(CENTER);
-    image(userIcon, 0, 0, 15, 15);
+    image(userIcon, 0, 0, 20, 20);
     pop();
   }
 }
@@ -154,7 +166,7 @@ let currentPlayingAudio = null;
 let isAudioPlaying = new Array(audioFiles.length).fill(false);
 
 function insideThePolygon(index) {
-  console.log("Inside Polygon: " + index);
+  // console.log("Inside Polygon: " + index);
 
   // Play audio corresponding to the polygon index
   if (audioFiles[index]) {
@@ -164,19 +176,21 @@ function insideThePolygon(index) {
       console.log("Failed to play audio: " + audioFiles[index]);
     }
   }
-    // // Stop the audio of other polygons if playing
-    // if (currentPlayingAudio !== null && currentPlayingAudio !== index) {
-    //   audioFiles[currentPlayingAudio].stop();
-    // }
-    // currentPlayingAudio = index;
+    // Stop the audio of other polygons if playing
+    if (currentPlayingAudio !== null && currentPlayingAudio !== index) {
+      audioFiles[currentPlayingAudio].stop();
+      isAudioPlaying[currentPlayingAudio] = false;
+    }
+    currentPlayingAudio = index;
 }
 
-function outsideThePolygon(index) {
-  console.log("Outside Polygon: " + index);
+function outsideThePolygon(index){
+  // console.log("Outside Polygon: " + index);
 
-  // Stop playing audio for the specific polygon index
-  if (audioFiles[index]) {
+  if(audioFiles[index]){
     audioFiles[index].stop();
+    isAudioPlaying[index] = false;
+    currentPlayingAudio = null;
   }
 }
 
@@ -193,7 +207,6 @@ function outsideThePolygon(index) {
 //   positionChanged(simulatedPosition); // update the position on the canvas
 // }
 
-
 let openButton = document.getElementById('open-button');
 let closeButton = document.getElementById('close-button');
 let popup = document.getElementById('popup');
@@ -209,5 +222,12 @@ closeButton.addEventListener('click', function() {
 let googleMapsLink = document.getElementById('google-maps-link');
 googleMapsLink.href = 'https://goo.gl/maps/9CdiX8tEcJZjqnqN9?coh=178572&entry=tt';
 
+// function simulatePositionChange(lat, lon) {
+//   let simulatedPosition = {
+//     latitude: lat,
+//     longitude: lon
+//   };
+//   positionChanged(simulatedPosition); // update the position on the canvas
+// }
 
 
